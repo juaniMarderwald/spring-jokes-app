83b58c3382d461b262f3924b9df83e97b44a518d
juaniMarderwald <jmarderwald87@gmail.com>
2023-03-06 15:40:59 -0300

    first commit

---
+++ src/main/java/guru/springframework/spring5jokes/appV2/Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokes.appV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
